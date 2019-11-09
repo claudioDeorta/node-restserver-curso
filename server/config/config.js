7aecfa94808cfae5c8b262fdcb0d70817b74159b
Claudio <claudiod86@hotmail.com>
2019-11-09 16:50:56 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://claudioD:iIooLYsPLaqWCJ8h@cluster0-7imxj.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
